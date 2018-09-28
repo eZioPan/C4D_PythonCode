d6169343005d1e625efd703b558f3266fabc2e5f
eZio Pan <eziopan@qq.com>
2018-09-28 20:22:24 +0800

    expand usage

--- MultiRayConnector_script.py
+++ MultiRayConnector_script.py
@@ -2,7 +2,7 @@
 # Author: eZioPan
 # Page: github.com/eZioPan
 # License: Creative Commons Attribution Share Alike 4.0
-# Last Update: 2018Sep27
+# Last Update: 2018Sep28
 
 """
 USEAGE:
@@ -25,13 +25,16 @@ def main():
     bc1[c4d.DESC_NAME] = "Ray Attribute"
     bc1[c4d.DESC_PARENTGROUP] = c4d.DescID(0)   # Put this group as a table of the object
 
-    [bc2,bc3] = [c4d.GetCustomDatatypeDefault(c4d.CUSTOMDATATYPE_INEXCLUDE_LIST) for i in range(0,2)]
-    bc2[c4d.DESC_NAME] = "From Objects List"
-    bc3[c4d.DESC_NAME] = "To Objects List"
+    [bc2, bc4] = [c4d.GetCustomDatatypeDefault(c4d.DTYPE_BOOL) for i in range(0,2)]
+    [bc3, bc5] = [c4d.GetCustomDatatypeDefault(c4d.CUSTOMDATATYPE_INEXCLUDE_LIST) for i in range(0,2)]
+    bc2[c4d.DESC_NAME] = "From Point"
+    bc3[c4d.DESC_NAME] = "From Objects List"
+    bc4[c4d.DESC_NAME] = "To Point"
+    bc5[c4d.DESC_NAME] = "To Objects List"
 
     DesID1 = sweepObj.AddUserData(bc1) # Get root user group description ID for other insertions
 
-    for item in [bc2, bc3]:
+    for item in [bc2, bc3, bc4, bc5]:
         item[c4d.DESC_PARENTGROUP] = DesID1
         sweepObj.AddUserData(item)
 
@@ -54,11 +57,52 @@ def main():
 # Author: eZioPan
 # Page: github.com/eZioPan
 # License: Creative Commons Attribution Share Alike 4.0
-# Last Update: 2018Sep26
+# Last Update: 2018Sep28
 
 import c4d
+from c4d.modules import mograph as mo
 
 lastState = False       # track last running state
+
+def GetGroupPointsPos(objLs=[], usePoint=False):
+    posLs = []
+    objCnt = len(objLs)
+    for i in range(objCnt):
+        obj = objLs.ObjectFromIndex(doc,i)
+        objMg = obj.GetMg()
+        if usePoint == False or obj.GetType() == c4d.Onull:
+             posLs.append(objMg.off)
+        elif obj.IsInstanceOf(c4d.Obasemogen):
+            md = mo.GeGetMoData(obj)
+            moMatrixLs = md.GetArray(c4d.MODATA_MATRIX)
+            for moMatrix in moMatrixLs:
+                point = moMatrix.off
+                toPosLs.append(objMg*point)
+        else:
+            pointPosLs = obj.GetCache().GetDeformCache().GetAllPoints()
+            for point in pointPosLs:
+                posLs.append(objMg*point)
+    return posLs
+
+def SetLinePointPos(lineObj, fromPosLs, toPosLs):
+    fromPosCnt = len(fromPosLs)
+    toPosCnt = len(toPosLs)
+    rayCnt = max(fromPosCnt, toPosCnt)
+    lineObj.ResizeObject(2*rayCnt, rayCnt)
+
+    for i in range(rayCnt):
+        lineObj.SetSegment(id=i, cnt=2, closed=False)
+
+    if fromPosCnt <= toPosCnt:
+        for i in range(toPosCnt):
+            pairNum = i % fromPosCnt
+            lineObj.SetPoint(i*2, fromPosLs[pairNum-1])
+            lineObj.SetPoint(i*2+1, toPosLs[i])
+    else:
+        for i in range(fromPosCnt):
+            pairNum = i % toPosCnt
+            lineObj.SetPoint(i*2, fromPosLs[i])
+            lineObj.SetPoint(i*2+1, toPosLs[pairNum-1])
 
 def main():
 
@@ -66,8 +110,10 @@ def main():
 
     sweepObj = op.GetObject()
     lineObj = sweepObj.GetDown().GetNext()
-    fromLs = sweepObj[c4d.ID_USERDATA,2]
-    toLs = sweepObj[c4d.ID_USERDATA,3]
+    fromPoint = sweepObj[c4d.ID_USERDATA,2]
+    fromLs = sweepObj[c4d.ID_USERDATA,3]
+    toPoint = sweepObj[c4d.ID_USERDATA,4]
+    toLs = sweepObj[c4d.ID_USERDATA,5]
 
     fromCnt = fromLs.GetObjectCount()
     toCnt = toLs.GetObjectCount()
@@ -84,36 +130,12 @@ def main():
         lastState = True
         print("[RayConnector] OK.")
 
-    fromMatrixLs = []
-    for i in range(fromCnt):
-        fromObj = (fromLs.ObjectFromIndex(doc,i))
-        fromMatrixLs.append(fromObj.GetMg().off)
+    fromPosLs = GetGroupPointsPos(fromLs,fromPoint)
 
-    toMatrixLs = []
-    for i in range(toCnt):
-        toObj = (toLs.ObjectFromIndex(doc,i))
-        toMatrixLs.append(toObj.GetMg().off)
+    toPosLs = GetGroupPointsPos(toLs,toPoint)
 
-    rayCnt = max(fromCnt, toCnt)
-    lineObj.ResizeObject(2*rayCnt, rayCnt)
 
-    for i in range(rayCnt):
-        lineObj.SetSegment(id=i, cnt=2, closed=False)
-
-    reverse = False
-    if fromCnt > toCnt:
-        reverse = True
-
-    if reverse == False:
-        for i in range(toCnt):
-            pairNum = i % fromCnt
-            lineObj.SetPoint(i*2, fromMatrixLs[pairNum-1])
-            lineObj.SetPoint(i*2+1, toMatrixLs[i])
-    else:
-        for i in range(fromCnt):
-            pairNum = i % toCnt
-            lineObj.SetPoint(i*2, fromMatrixLs[i])
-            lineObj.SetPoint(i*2+1, toMatrixLs[pairNum-1])
+    SetLinePointPos(lineObj, fromPosLs, toPosLs)
 
     lineObj.Message(c4d.MSG_UPDATE)     # Update points change of object
 
